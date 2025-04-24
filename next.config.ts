6def42db290e889c5bce69debcd1a683b147867b
jaskinty <jorahty@gmail.com>
2025-04-23 22:14:59 -0700

    init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: '/index.html',
+      },
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
